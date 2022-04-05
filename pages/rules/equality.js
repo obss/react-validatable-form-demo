6a52fac1310e5e4e0488e2f3bd7d0eb9cfe1b1c9
Enes Malik Acun <37189443+maliksenpai@users.noreply.github.com>
2022-04-05 14:29:41 +0300

    - updated equality page (#15)
    - updated equality page

--- pages/rules/equality.js
+++ pages/rules/equality.js
@@ -3,7 +3,15 @@ import ExampleUsageWrapper from '../../components/ExampleUsageWrapper';
 import TextField from '@mui/material/TextField';
 import ValidationResult from '../../components/ValidationResult';
 import CurrentRulesInfo from '../../components/CurrentRulesInfo';
-import { Autocomplete, Checkbox, FormControl, FormControlLabel, FormGroup, FormHelperText } from '@mui/material';
+import {
+    Autocomplete,
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    FormGroup,
+    FormHelperText,
+    Typography
+} from '@mui/material';
 import { options } from "../../constants/Data";
 
 
@@ -50,67 +58,73 @@ const Equality = () => {
                 <b>equality</b> rule checks if the given value is equal to comparison value.
             </p>
             <div>
-                <FormGroup className={'checkboxOnRight'}>
-                    <FormControl error={!!getError('val')}>
-                        <FormControlLabel
-                            control={
-                                <Checkbox
-                                    checked={getValue('val') || false}
-                                    onChange={(e) => setPathValue('val', e.target.checked)}
-                                />
-                            }
-                            label={<FormHelperText>{getError('val') || ' '}</FormHelperText>}
-                        />
-                    </FormControl>
-                </FormGroup>
+                <Typography className={"comparisonArea"} variant={'h6'}> equalTo </Typography>
+                <div>
+                    <FormGroup className={'checkboxOnRight'}>
+                        <FormControl error={!!getError('val')}>
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        checked={getValue('val') || false}
+                                        onChange={(e) => setPathValue('val', e.target.checked)}
+                                    />
+                                }
+                                label={<FormHelperText>{getError('val') || ' '}</FormHelperText>}
+                            />
+                        </FormControl>
+                    </FormGroup>
+                </div>
+                <div className="comparisonDiv">
+                    <TextField
+                        error={!!getError('val2')}
+                        helperText={getError('val2') || ' '}
+                        label="valueEqualToVal"
+                        type="text"
+                        value={getValue('val2') || ''}
+                        onChange={(e) => setPathValue('val2', e.target.value)}
+                    />
+                    <TextField
+                        className="comparisonComponent"
+                        label="comparisonValue"
+                        type="text"
+                        value={getValue('comparisonValue')}
+                        onChange={(e) => setPathValue('comparisonValue', e.target.value)}
+                    />
+                </div>
             </div>
-            <div className="comparisonDiv">
-                <TextField
-                    error={!!getError('val2')}
-                    helperText={getError('val2') || ' '}
-                    label="val2"
-                    type="text"
-                    value={getValue('val2') || ''}
-                    onChange={(e) => setPathValue('val2', e.target.value)}
-                />
-                <TextField
-                    className="comparisonComponent"
-                    label="comparisonValue"
-                    type="text"
-                    value={getValue('comparisonValue')}
-                    onChange={(e) => setPathValue('comparisonValue', e.target.value)}
-                />
-            </div>
-            <div className="comparisonDiv">
-                <TextField
-                    error={!!getError('valIsOneOf1')}
-                    helperText={getError('valIsOneOf1') || ' '}
-                    label="valIsOneOf1"
-                    type="text"
-                    value={getValue('valIsOneOf1') || ''}
-                    onChange={(e) => setPathValue('valIsOneOf1', e.target.value)}
-                />
-                {"['North America', 'Africa', 'Europe']"}
-            </div>
-            <div className="comparisonDiv">
-                <TextField
-                    error={!!getError('valIsOneOf2')}
-                    helperText={getError('valIsOneOf2') || ' '}
-                    label="valIsOneOf2"
-                    type="text"
-                    value={getValue('valIsOneOf2') || ''}
-                    onChange={(e) => setPathValue('valIsOneOf2', e.target.value)}
-                />
-                <Autocomplete
-                    multiple
-                    value={getValue('comparisonValueIsOneOf')}
-                    onChange={(event, newValue) => {
-                        setPathValue('comparisonValueIsOneOf', newValue);
-                    }}
-                    options={options}
-                    renderInput={(params) => <TextField {...params} label="ruleOption" />}
-                />
-            </div>
+           <div>
+               <Typography className={"comparisonArea"} variant={'h6'}> isOneOf </Typography>
+               <div className="comparisonDiv">
+                   <TextField
+                       error={!!getError('valIsOneOf1')}
+                       helperText={getError('valIsOneOf1') || ' '}
+                       label="isOneOf"
+                       type="text"
+                       value={getValue('valIsOneOf1') || ''}
+                       onChange={(e) => setPathValue('valIsOneOf1', e.target.value)}
+                   />
+                   {"['North America', 'Africa', 'Europe']"}
+               </div>
+               <div className="comparisonDiv">
+                   <TextField
+                       error={!!getError('valIsOneOf2')}
+                       helperText={getError('valIsOneOf2') || ' '}
+                       label="isOneOfFunc"
+                       type="text"
+                       value={getValue('valIsOneOf2') || ''}
+                       onChange={(e) => setPathValue('valIsOneOf2', e.target.value)}
+                   />
+                   <Autocomplete
+                       multiple
+                       value={getValue('comparisonValueIsOneOf')}
+                       onChange={(event, newValue) => {
+                           setPathValue('comparisonValueIsOneOf', newValue);
+                       }}
+                       options={options}
+                       renderInput={(params) => <TextField {...params} label="ruleOption" />}
+                   />
+               </div>
+           </div>
             <ValidationResult isValid={isValid} />
             <CurrentRulesInfo currentRules={rules} />
         </ExampleUsageWrapper>
