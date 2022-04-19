3e2f3b057fae5d5278cba812dc64fed915ca30f9
ahmetemrekilinc <aekilinc@gmail.com>
2022-04-20 00:12:47 +0300

    3.4.0

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
     env: {
-        VERSION: '3.2.1',
+        VERSION: '3.4.0',
     },
     images: {
         loader: 'akamai',
