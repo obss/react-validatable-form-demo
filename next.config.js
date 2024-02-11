bf93426499b1d06c8e08b56118b98315d811d67c
ahmetemrekilinc <aekilinc@gmail.com>
2024-02-11 19:45:18 +0300

    5.0.2

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
     env: {
-        VERSION: '5.0.1',
+        VERSION: '5.0.2',
     },
 };
 
